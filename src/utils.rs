1853958189080f20604a98d6b15f84d8c20f5508
2er0 <2er0@dbaumi.at>
2024-12-02 09:30:00 +0100

    fixed serve url and crypto on non https

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 use std::iter;
 
 pub fn id_generator(len: usize) -> String {
-    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNPQRSTUVWXYZ0123456789";
     let mut rng = rand::thread_rng();
     let one_char = || CHARSET[rng.gen_range(0..CHARSET.len())] as char;
     iter::repeat_with(one_char).take(len).collect()
